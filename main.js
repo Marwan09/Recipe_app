9d0e9d99837e02464aa355a6ba24f70dd03292ef
Marwan09 <75195799+Marwan09@users.noreply.github.com>
2024-01-04 05:21:38 +0100

    2

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const recipe_object = {
     recipe_name: "Chocolate Raspberry Brownies",
     recipe_image_url: "image/Chocolate Raspberry Brownies.jpg",
   
+    
     areas: "America",
     ingridients: {
       ingredient_name: ["Dark Chocolate", "Milk Chocolate", "Salted Butter", "Light Brown Soft Sugar", "Eggs", "Plain Flour", "Cocoa", "Raspberries",],
@@ -11,12 +12,15 @@ const recipe_object = {
   
   
     
+
+
   
   method_para:
   "Heat oven to 180C/160C fan/gas 4. Line a 20 x 30cm baking tray tin with baking parchment. Put the chocolate, butter and sugar in a pan and gently melt, stirring occasionally with a wooden spoon. Remove from the heat. Stir the eggs, one by one, into the melted chocolate mixture. Sieve over the flour and cocoa, and stir in. Stir in half the raspberries, scrape into the tray, then scatter over the remaining raspberries. Bake on the middle shelf for 30 mins or, if you prefer a firmer texture, for 5 mins more. Cool before slicing into squares. Store in an airtight container for up to 3 days",
     youtube_link: "https://www.youtube.com/embed/Pi89PqsAaAg?si=pUKpGX0a5oGKijID",
   };
   
+
   const recipe_name = document.querySelector(".title");
   const method_para = document.querySelector(".method_para");
   const recipe_areas = document.querySelector(".areas");
@@ -44,6 +48,8 @@ const recipe_object = {
                                                       
                                     
   })
+  
+  
   
   // ing_list.style.backgroundColor="red"
   
